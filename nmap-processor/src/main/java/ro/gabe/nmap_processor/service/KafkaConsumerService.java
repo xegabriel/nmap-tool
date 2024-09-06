3b77e0581b4cce4b9fbd86af701ebcf00c6aadec
Gabriel <26674283+xegabriel@users.noreply.github.com>
2024-09-06 20:15:22 +0300

    [NMAP-7] Implemented multithreading target processing

--- nmap-processor/src/main/java/ro/gabe/nmap_processor/service/KafkaConsumerService.java
+++ nmap-processor/src/main/java/ro/gabe/nmap_processor/service/KafkaConsumerService.java
@@ -1,5 +1,9 @@
 package ro.gabe.nmap_processor.service;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -11,10 +15,28 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumerService {
 
   private final ScanService scanService;
+  // Reuse a single executor service for all tasks
+  private final ExecutorService executor = Executors.newFixedThreadPool(10);
 
   @KafkaListener(topics = "scan-results", groupId = "scan-consumers")
-  public void consumeScanResult(String target) {
+  public void consumeTargetToBeScanned(String target) {
     log.info("Consumed message: {}", target);
-    scanService.performScan(target);
+    executor.submit(() -> {
+      scanService.performScan(target);
+    });
+  }
+
+  @PreDestroy
+  public void shutdownExecutor() {
+    log.info("Shutting down executor service");
+    executor.shutdown();
+    try {
+      if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+        executor.shutdownNow();
+      }
+    } catch (InterruptedException e) {
+      executor.shutdownNow();
+      Thread.currentThread().interrupt();
+    }
   }
 }
